002702d04c462ced768c652ffdaaa15a7fab9b15
Sumith Kollipara <sumith_kollipara@yahoo.com>
2023-04-06 10:47:53 +0200

    Code implementation for pyramid logic

--- Pyramid/Program.cs
+++ Pyramid/Program.cs
@@ -6,8 +6,25 @@ namespace Pyramid
     {
         static void Main(string[] args)
         {
-                   
+            Console.WriteLine("Please enter the number: ");
+
+            var isValidNumber = int.TryParse(Console.ReadLine(), out int value);
+            if (isValidNumber)
+                Pyramid(Convert.ToInt32(value));
+            else
+                Console.WriteLine("Entered value is not a valid number.");
+
+            Console.ReadLine();            
         }
-       
+
+        private static void Pyramid(int height)
+        {
+            for (int i = 0; i < height; i++)
+            {
+                Console.Write(new String(' ', height - i - 1));
+                Console.Write(new String('*', i * 2 + 1));
+                Console.WriteLine();
+            }
+        }
     }
 }
